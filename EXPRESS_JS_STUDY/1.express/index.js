35f340c98ba9f816553008b2d5d29c0a8d616500
irfan701 <irfanhossain701@gmail.com>
2022-09-24 01:35:15 +0400

    express.static
    express.static

--- EXPRESS_JS_STUDY/1.express/index.js
+++ EXPRESS_JS_STUDY/1.express/index.js
@@ -1,8 +1,13 @@
-const express=require('express')
+const express=require('express') //function
 
-const app=express()
+const app=express()  // obj
 
 app.use(express.json())
+app.use(express.static(`${__dirname}/public/`))// http://localhost:3000/text/text.txt
+app.use(express.static(`${__dirname}/public/`,{
+    index:'home.html'
+}))// http://localhost:3000/text
+
 
 app.get('/',(req,res)=>{
     res.send("Hello Programers")   
@@ -12,7 +17,7 @@ app.get('/',(req,res)=>{
 
 app.post('/',(req,res)=>{
     res.send("This is Home Page With Post Request")
-    console.log(req.body) 
+    console.log(req.body)  //type of = object
     console.log(req.body.name) 
 })
 
